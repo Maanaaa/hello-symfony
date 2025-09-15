035ae8e40218f8d93d42247666bcf55cb30a0595
Théo <manya.th@icloud.com>
2025-09-15 20:27:46 +0200

    feat(home): handle query parameters with Request object

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,16 +1,16 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Reponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController
 {
     #[Route("/", name: "home")]
-    function index(): Response
+    function index(Request $request): Response
     {
-        return new Response("Hello, Symfony!");
+        return new Response("Hello, " . $request->query->get('name', 'Symfony!'));
     }
 }
 
