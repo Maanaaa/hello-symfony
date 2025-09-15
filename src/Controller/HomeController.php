9c42d3148794434c885cedfeb7cba13e32bb8458
Théo <manya.th@icloud.com>
2025-09-15 22:13:01 +0200

    feat(home): render Twig template with title variable

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,21 +2,21 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController
+
+class HomeController extends AbstractController
 {
     #[Route("/", name: "home")]
-    #[Route("/code/{slug}--{id}", name: "code.show", requirements: ["id" => "\d+", "slug" => "[a-z0-9\-]+"])]
-    public function index(Request $request, $slug, $id): Response
+    public function index(Request $request): Response
     {
-        if ($slug && $id) {
-            return new Response("Slug: $slug | ID: $id");
-        }
+        // First Twig Page
 
-        $name = $request->query->get('name', 'Anonyme');
-        return new Response("Bonjour $name");
+        return $this->render('home/index.html.twig', [
+            'title' => 'Welcome to my first Symfony',
+        ]);
     }
 }
